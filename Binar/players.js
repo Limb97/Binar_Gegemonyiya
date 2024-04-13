366d355d01dae77451e30d5235435258778bc698
Limb97 <166938683+Limb97@users.noreply.github.com>
2024-04-13 18:27:16 +0200

    Delete Binar directory

--- Binar/players.js
+++
@@ -1,8 +0,0 @@
-console.log("Скрипт странички игроков успешно загружен");
-
-const numPlayer = document.querySelectorAll(".num-pl");
-
-
-numPlayer.forEach((item, i) => {
-    item.innerHTML = i + 1
-});
